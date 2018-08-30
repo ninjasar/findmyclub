4a304b38743c068759a5412e4fca5e6457ae17e4
Jinren Ping <jinren.ping@outlook.com>
2018-08-30 11:49:41 -0400

    Update landing page

--- src/route-containers/LoginLanding.js
+++ src/route-containers/LoginLanding.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Networking from '../util/Networking';
 
 import Logo from '../images/FindMyClub_Logo.svg';
+import Promotion from '../images/findMyClubPromotion.png'
 import '../css/containers/LoginLanding.css';
 
 class LoginLanding extends Component {
@@ -20,17 +21,17 @@ class LoginLanding extends Component {
     *                           *
     *****************************/
 
-	render() {
-		return (
-			<div className="LoginLanding container">
-				<img src={Logo} alt='Find My Club' className='login-landing-logo'/>
-
-                <button className='round-rect-button login-landing-login-btn'
-                        onClick={this.handleLogin.bind(this)}>Login</button>
-                <p className='login-landing-label'>New York University</p>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="LoginLanding container">
+                <img src={Logo} alt="Find My Club" className="login-landing-logo" />
+                <div className="login-landing-text">Find clubs that are right just for you</div>
+                <button className="login-landing-login-btn"
+                    onClick={this.handleLogin.bind(this)}>Login</button>
+                <img src={Promotion} alt="Promotion Image" className="login-landing-promotion" />
+            </div>
+        );
+    }
 
 
 	/****************************
