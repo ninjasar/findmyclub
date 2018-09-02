9511586e6586e11be411e373e99814ee5e95f60c
Mingfei Huang <himax1023@gmail.com>
2018-09-02 00:53:57 -0400

    remove service worker for now just to deploy this thing

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -5,5 +5,6 @@ export default {
     CONTAINER_TRANSITION_TIME: 250,
     OVERLAY_TRANSITION_TIME: 350,
     clubThumbnailDefaultPath: thumbnailUrl,
-    BASE_URL: 'https://dev.findmyclub.nyu.edu' /*'https://d1hmwr991s6qf2.cloudfront.net'*/ /*'https://findmyclub.nyu.edu/'*/
+    // BASE_URL: 'https://dev.findmyclub.nyu.edu' /*'https://d1hmwr991s6qf2.cloudfront.net'*/ 
+    BASE_URL: 'https://findmyclub.nyu.edu',
 };
