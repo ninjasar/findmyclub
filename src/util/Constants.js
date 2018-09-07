f111e9128471e72849a9e53fb0974f2f6472322b
Mingfei Huang <himax1023@gmail.com>
2018-09-07 11:14:11 -0400

    use env var for base url

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -5,6 +5,5 @@ export default {
     CONTAINER_TRANSITION_TIME: 250,
     OVERLAY_TRANSITION_TIME: 350,
     clubThumbnailDefaultPath: thumbnailUrl,
-    // BASE_URL: 'https://dev.findmyclub.nyu.edu' /*'https://d1hmwr991s6qf2.cloudfront.net'*/ 
-    BASE_URL: 'https://findmyclub.nyu.edu',
+    BASE_URL: process.env.REACT_APP_API_URL,
 };
