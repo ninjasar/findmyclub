ab806e86d7b1f6d2ec641c38743eea5ff3394b03
Adeola Uthman <authman2@gmail.com>
2018-09-11 14:55:26 -0400

    Add more voiceover stuff

--- src/util/Maps.js
+++ src/util/Maps.js
@@ -9,18 +9,22 @@ const mCtC = ({ image, ID, Name, tags, tagColor, followed, interest }, onClubCli
     return (
         <div className='club-item' key={ID}>
             <div className='club-card'>
-                <div className='club-card-top' onClick={() => { onClubClick({ ID, Name, tags,
-                                                                            image, tagColor, followed,
-                                                                            interest }) }}>
-                    <div className='club-card-image-area'>
+                <div className='club-card-top' 
+                    onClick={() => { onClubClick({ ID, Name, tags,
+                        image, tagColor, followed,
+                        interest }) 
+                    }}
+                    role='region' aria-live='Club Card' aria-label={`Club Title is ${Name}. 
+                                                                    The associated interest is ${interest}.`}>
+                    <div className='club-card-image-area' aria-hidden={true}>
                         <img src={image} alt='club-preview' className='club-card-image'/>
                     </div>
 
-                    <h3 className='club-card-title'>{Name.substring(0, 30)}{Name.length > 30 && '...'}</h3>
+                    <h3 className='club-card-title' aria-hidden={true}>{Name.substring(0, 30)}{Name.length > 30 && '...'}</h3>
                     <div className='club-card-tags' style={{
                         backgroundColor: tagColor 
-                    }}></div>
-                    <p className='club-card-tags-label'>{interest}</p>
+                    }} aria-hidden={true}></div>
+                    <p className='club-card-tags-label' aria-hidden={true}>{interest}</p>
                 </div>
                 <div className='club-card-follow-button' 
                     onClick={() => { onFollowClick({ ID, Name, tags,
@@ -94,8 +98,12 @@ export default {
         const name = interest.Name;
         return (
             <div className='login-club-matches-category-section' key={name}>
-                <h1 className='login-club-matches-category-section-title'>{name}</h1>
-                <h1 className='login-club-matches-category-section-subtitle'>
+                <h1 className='login-club-matches-category-section-title'
+                    role='region' aria-live='Interest Title' aria-label={`Interest Title is ${name}`}>
+                    {name}
+                </h1>
+                <h1 className='login-club-matches-category-section-subtitle'
+                    role='region' aria-live='Number of Clubs'>
                     We found <span style={{ color: interest.Color }}>{clubs.length} club(s)</span> that match your interest.
                 </h1>
 
