fa79fd36312ad891c97b8046021961d35eca5fde
ninjasar <sarahpierce410@gmail.com>
2018-09-28 15:59:28 -0400

    added aria on search loading page

--- src/route-containers/LoginMatching.js
+++ src/route-containers/LoginMatching.js
@@ -25,8 +25,8 @@ class LoginMatching extends Component {
 		return (
 			<div className="LoginMatching container" role='region' aria-label='Sit tight while we find some clubs for you!'>
                 <LoadingBubbles />
-                <h2>Searching for the perfect match for you.</h2>
-                <p>This may take a few seconds. Please do not exit or refresh the page. </p>
+                <h2 aria-label='Searching for the perfect match for you.' tabIndex={0}>Searching for the perfect match for you.</h2>
+                <p aria-label='This may take a few seconds. Please do not exit or refresh the page.' tabIndex={0}>This may take a few seconds. Please do not exit or refresh the page. </p>
 			</div>
 		);
 	}
